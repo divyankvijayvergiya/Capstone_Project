2eb8f3602c4e606325dceba5e7b238796d93b78a
Divyank Vijayvergiya <divyankvijay.96@gmail.com>
2017-09-20 02:08:54 +0530

    first commit

---
+++ app/src/main/java/application/example/com/notecard/Users.java
@@ -0,0 +1,41 @@
+package application.example.com.notecard;
+
+/**
+ * Created by Dell on 20-09-2017.
+ */
+
+public class Users {
+
+    private String photoUrl;
+    private String name;
+    private String email;
+    public Users(String photoUrl, String name, String email){
+        this.photoUrl=photoUrl;
+        this.name=name;
+        this.email=email;
+    }
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
