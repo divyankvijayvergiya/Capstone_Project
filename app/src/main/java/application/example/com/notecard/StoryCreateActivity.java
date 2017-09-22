f3b4073979c45dfad83f413be9ef71f430615de8
Divyank Vijayvergiya <divyankvijay.96@gmail.com>
2017-09-23 02:06:03 +0530

    seventh commit

--- app/src/main/java/application/example/com/notecard/StoryCreateActivity.java
+++ app/src/main/java/application/example/com/notecard/StoryCreateActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -25,7 +26,7 @@ import java.util.Map;
  * Created by Dell on 23-09-2017.
  */
 
-public class StoryCreateActivity extends MainActivity implements View.OnClickListener {
+public class StoryCreateActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText etTitle;
     private EditText etNote;
     private ImageButton btDone;
